2dd81a63282381033d5e1d718e0579ebe03f5c50
Setu Shah <snshah12@asu.edu>
2019-05-02 01:49:59 -0700

    second commit

---
+++ cnn.py
@@ -0,0 +1,44 @@
+
+from keras.preprocessing.image import img_to_array
+from keras.models import load_model
+import numpy as np
+import argparse
+import imutils
+import pickle
+import cv2
+import os
+
+
+img_dir="static/"
+
+def predict(image_id):
+    image_path=img_dir+str(image_id)
+    image = cv2.imread(image_path)
+    
+    output = imutils.resize(image, width=400)
+    image = cv2.resize(image, (96, 96))
+    image = image.astype("float") / 255.0
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis=0)
+    print("[INFO] loading network...")
+    model = load_model("fashion.model")
+    mlb = pickle.loads(open( "mlb.pickle", "rb").read())
+    proba = model.predict(image)[0]
+    idxs = np.argsort(proba)[::-1][:2]
+    for (i, j) in enumerate(idxs):
+        # build the label and draw the label on the image
+    	 label = "{}: {:.2f}%".format(mlb.classes_[j], proba[j] * 100)
+    	 cv2.putText(output, label, (10, (i * 30) + 25), 
+    		cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+	 
+
+    # show the probabilities for each of the individual labels
+    for (label, p) in zip(mlb.classes_, proba):
+    	print("{}: {:.2f}%".format(label, p * 100))
+    
+    # show the output image
+    cv2.imshow("Output", output)
+    cv2.waitKey(0)
+
+
+
